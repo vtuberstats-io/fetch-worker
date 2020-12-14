5a35cf2124f2d13a28b1abb8daa197f0fa4a9e9f
darkyzhou <darkyoooooo@gmail.com>
2020-12-14 23:36:01 +0800

    wip: making progress

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 'use strict';
 
-require('dotenv').config();
-
 const YOUTUBE_API_KEY = process.env.YOUTUBE_API_KEY;
 const KAFKA_BROKERS = process.env.KAFKA_BROKERS;
 const CONCURRENCY = Number(process.env.CONCURRENCY) || 8;
@@ -23,7 +21,7 @@ if (!HOSTNAME) {
 
 const { google } = require('googleapis');
 const { Kafka } = require('kafkajs');
-const { default: PQueue } = require('p-queue');
+const AutoRetryQueue = require('./lib/queue');
 
 const TOPIC_FETCH_TASK_SCHEDULE = 'fetch-task-schedule';
 const TOPIC_FETCH_TASK_DONE = 'fetch-task-done';
@@ -35,20 +33,81 @@ const kafka = new Kafka({
 });
 const fetchTaskScheduleConsumer = kafka.consumer({ groupId: 'fetch-worker' });
 const fetchTaskResultProducer = kafka.producer();
-const fetchTaskQueue = new PQueue({ concurrency: CONCURRENCY });
+const fetchTaskQueue = new AutoRetryQueue(CONCURRENCY, MAX_RETRY);
 let taskResultsJson = [];
 
 async function init() {
   setInterval(() => pushFinishedTasksToKafka(), 1000 * 60);
 
-  console.log(`connecting to kafka with brokers: ${KAFKA_BROKERS}`);
+  console.info(`connecting to kafka with brokers: ${KAFKA_BROKERS}`);
   await fetchTaskResultProducer.connect();
   await fetchTaskScheduleConsumer.connect();
   await fetchTaskScheduleConsumer.subscribe({ topic: TOPIC_FETCH_TASK_SCHEDULE });
-  await doReadScheduleFromKafka();
+
+  console.info('start reading scheduled tasks')
+  await doReadTaskFromKafka();
 }
 
 init().catch((err) => console.error(err));
+
+async function doReadTaskFromKafka() {
+  await fetchTaskScheduleConsumer.run({
+    eachMessage: async ({ message }) => {
+      const fetchTask = JSON.parse(message.value.toString());
+      let hit = false;
+      if (fetchTask.vtuberMeta.youtubeChannelId) {
+        hit = true;
+        console.info(`queued youtube fetching task '${message.value.toString()}'`);
+        doFetchTask(fetchTask, 'youtube');
+      }
+      if (fetchTask.vtuberMeta.bilibiliChannelId) {
+        hit = true;
+        console.info(`queued bilibili fetching task '${message.value.toString()}'`);
+        doFetchTask(fetchTask, 'bilibili');
+      }
+      if (!hit) {
+        console.warn(`task '${message.value.toString()}' has no valid channel ids, skipping`);
+      }
+    }
+  });
+}
+
+function doFetchTask(task, type) {
+  fetchTaskQueue.enqueue(
+    async () => {
+      taskResultsJson.push(
+        JSON.stringify({
+          scheduledTimestamp: task.scheduledTimestamp,
+          type,
+          vtuberMeta: task.vtuberMeta,
+          data:
+            type === 'youtube'
+              ? await fetchFromYoutube(task.vtuberMeta.youtubeChannelId)
+              : await fetchFromBilibili(task.vtuberMeta.bilibiliChannelId)
+        })
+      );
+    },
+    (e) =>
+      console.error(
+        `${type} fetching task for '${task.vtuberMeta.vtuberId}' has failed for too many times(${MAX_RETRY}), latest error: '${e}'`
+      )
+  );
+}
+
+async function fetchFromYoutube(channelId) {
+  const response = await youtubeApi.channels.list({
+    id: channelId,
+    part: 'id,statistics,brandingSettings'
+  });
+  if (!response?.data?.items) {
+    throw new Error(`invalid youtube response: ${JSON.stringify(response)}`);
+  }
+  return response.data.items[0];
+}
+
+async function fetchFromBilibili(channelId) {
+  // TODO
+}
 
 async function pushFinishedTasksToKafka() {
   if (taskResultsJson.length <= 0) {
@@ -58,92 +117,16 @@ async function pushFinishedTasksToKafka() {
   taskResultsJson = [];
   try {
     await fetchTaskResultProducer.send({
+      acks: -1,
       topic: TOPIC_FETCH_TASK_DONE,
       messages: results.map((r) => ({ value: r }))
     });
     console.info(`pushed ${results.length} task results to kafka`);
   } catch (e) {
+    taskResultsJson = [...taskResultsJson, ...results];
     console.error(
       `failed to push ${results.length} task results to kafka, will retry in the next run`
     );
     console.error(e);
   }
-}
-
-async function doReadScheduleFromKafka() {
-  await fetchTaskScheduleConsumer.run({
-    eachMessage: async ({ message }) => {
-      console.info(`queued task: ${message.value.toString()}`);
-      const vtuberMeta = JSON.parse(message.value.toString());
-      vtuberMeta.retryCount = 0;
-      if (vtuberMeta.youtubeChannelLinks) {
-        handleFetchTaskFromYoutube(vtuberMeta);
-      }
-      if (vtuberMeta.bilibiliChannelLinks) {
-        handleFetchTaskFromBilibili(vtuberMeta);
-      }
-    }
-  });
-}
-
-function handleFetchTaskFromYoutube(vtuberMeta) {
-  queueFetchTask(async () => {
-    taskResultsJson.push(
-      JSON.stringify({
-        type: 'youtube',
-        data: await fetchFromYoutube(vtuberMeta.youtubeChannelId)
-      })
-    );
-  });
-}
-
-function handleFetchTaskFromBilibili(vtuberMeta) {
-  queueFetchTask(async () => {
-    taskResultsJson.push(
-      JSON.stringify({
-        type: 'bilibili',
-        data: await fetchFromBilibili(vtuberMeta.bilibiliChannelId)
-      })
-    );
-  });
-}
-
-async function fetchFromYoutube(channelId) {
-  const response = await youtubeApi.channels.list({
-    id: channelId,
-    part: 'id,statistics,brandingSettings'
-  });
-  if (!response?.data?.items) {
-    throw new Error(`invalid response: ${JSON.stringify(response)}`);
-  }
-  return response.data.items[0];
-}
-
-async function fetchFromBilibili(channelId) {
-  // TODO
-}
-
-function queueFetchTask(task) {
-  const handler = async (meta) => {
-    try {
-      await task();
-    } catch (e) {
-      if (meta.retryCount >= MAX_RETRY) {
-        console.error(
-          `task '${JSON.stringify(
-            meta
-          )}' has failed for too many times(${MAX_RETRY}), latest error: '${e}'`
-        );
-      } else {
-        meta.retryCount++;
-        console.warn(
-          `task '${JSON.stringify(meta)}' failed due to error: '${e}', retrying (${
-            meta.retryCount
-          }/${MAX_RETRY})`
-        );
-        fetchTaskQueue.add(() => handler(meta));
-      }
-    }
-  };
-  fetchTaskQueue.add(() => handler(vtuberMeta));
 }
